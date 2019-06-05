f3c74864c0e194452babcae9afe74451a2b28f1b
Eric J. Humphrey <ejhumphrey@spotify.com>
2019-06-05 19:14:36 -0400

    Attempting to avoid hostchecking

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 
-scp -i id_rsa -P 2222 -rv ./docs/_site/* ${DEPLOY_USER}@${DEPLOY_HOST}:~/public_html/
+scp -o "StrictHostKeyChecking no" -i id_rsa -P 2222 -rv ./docs/_site/* ${DEPLOY_USER}@${DEPLOY_HOST}:~/public_html/
