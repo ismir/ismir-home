67f91e7081f7d0fff4524f23080167e8876c6da8
Eric J. Humphrey <ejhumphrey@spotify.com>
2019-06-05 19:26:47 -0400

    Added chmod

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -ev
 
-scp -o "StrictHostKeyChecking no" -i id_rsa -P 2222 -rv ./docs/_site/* ${DEPLOY_USER}@${DEPLOY_HOST}:~/public_html/
+chmod 600 id_rsa
+scp -o "StrictHostKeyChecking no" \
+	-i id_rsa \
+	-P 2222 \
+	-rv ./docs/_site/* \
+	${DEPLOY_USER}@${DEPLOY_HOST}:~/public_html/
